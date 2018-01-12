0ff10b7ff266a0dfb47f3205cc2cf1feb4d0e0be
janovrom <roman.janovsky9@seznam.cz>
2018-01-12 14:13:44 +0100

    shadowmap header

---
+++ raymarching/src/shadowmap.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#ifndef __SHADOW_MAP_H__
+#define __SHADOW_MAP_H__
+
+class ShadowMap
+{
+public:
+	ShadowMap();
+	~ShadowMap();
+
+	bool Init();
+
+};
+
+#endif // !__SHADOW_MAP_H__
